922b54578b8a9fa0218da27312c765475d16ff73
Xaice01 <103635912+Xaice01@users.noreply.github.com>
2023-08-17 14:18:48 +0200

    création de la page du quiz sans les données

--- app/src/main/java/com/example/superquiz/MainActivity.java
+++ app/src/main/java/com/example/superquiz/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.superquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+
+import com.example.superquiz.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
     }
 }
--- app/src/main/java/com/example/superquiz/WelcomeFragment.java
+++ app/src/main/java/com/example/superquiz/WelcomeFragment.java
@@ -2,11 +2,19 @@ package com.example.superquiz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.superquiz.databinding.FragmentWelcomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,20 +23,55 @@ import android.view.ViewGroup;
  */
 public class WelcomeFragment extends Fragment {
 
+    private FragmentWelcomeBinding binding;
+
+
     public static WelcomeFragment newInstance() {
-        WelcomeFragment fragment = new WelcomeFragment();
-        return fragment;
+        return new WelcomeFragment();
+
+    }
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(false);
+        binding.usernameEditText.addTextChangedListener(new TextWatcher(){
+          @Override
+          public void beforeTextChanged(CharSequence s, int start, int count, int after){
+
+          }
+
+          @Override
+          public void onTextChanged(CharSequence s, int Start, int before, int count){
+
+          }
+
+          @Override
+          public void afterTextChanged(Editable s){
+            binding.playButton.setEnabled(!s.toString().isEmpty());
+          }
+        });
+
+        binding.playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //The user just clicked
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                QuizFragment quizFragment = QuizFragment.newInstance();
+                fragmentTransaction.replace(R.id.container, quizFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
-    }
 }
