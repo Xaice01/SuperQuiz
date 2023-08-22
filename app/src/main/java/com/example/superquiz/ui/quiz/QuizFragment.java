10ae58631a63de11ab23d9de135992efbda49d0d
Xaice01 <103635912+Xaice01@users.noreply.github.com>
2023-08-22 13:23:06 +0200

    fonction on click des buton answer and inisialisation des question (a ma facon)

--- app/src/main/java/com/example/superquiz/ui/quiz/QuizFragment.java
+++ app/src/main/java/com/example/superquiz/ui/quiz/QuizFragment.java
@@ -1,16 +1,31 @@
 package com.example.superquiz.ui.quiz;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.superquiz.R;
+import com.example.superquiz.data.Question;
 import com.example.superquiz.data.QuestionBank;
 import com.example.superquiz.data.QuestionRepository;
+import com.example.superquiz.databinding.FragmentQuizBinding;
+import com.example.superquiz.databinding.FragmentWelcomeBinding;
+import com.example.superquiz.injection.ViewModelFactory;
+
 
 
 /**
@@ -20,10 +35,15 @@ import com.example.superquiz.data.QuestionRepository;
  */
 public class QuizFragment extends Fragment {
 
-    private QuizViewModel viewModel = new QuizViewModel(
-            new QuestionRepository(
-        new QuestionBank())
-    );
+    private QuizViewModel viewModel;
+    private FragmentQuizBinding binding;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
 
     public static QuizFragment newInstance() {
@@ -31,15 +51,98 @@ public class QuizFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentQuizBinding.inflate(inflater, container, false);
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+       // return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(QuizViewModel.class);
+
+        viewModel.startQuiz();
+
+        viewModel.currentQuestion.observe(getViewLifecycleOwner(), new Observer<Question>() {
+            @Override
+            public void onChanged(Question question) {
+
+                updateQuestion(question);
+
+            }
+        });
+
+        binding.answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewModel.isAnswerValid(0)){
+                    binding.answer1.setBackgroundColor(Color.parseColor("#388e3c"));  //vert de réponse
+                    binding.validityText.setText("💪 Good Answer !");
+                }else{
+                    binding.answer1.setBackgroundColor(Color.RED);                             //rouge de réponse
+                    binding.validityText.setText("Bad answer 😢");
+                }
+                binding.validityText.setVisibility(View.VISIBLE);
+                binding.next.setVisibility(View.VISIBLE);
+            }
+        });
+        binding.answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewModel.isAnswerValid(1)){
+                    binding.answer2.setBackgroundColor(Color.parseColor("#388e3c"));  //vert de réponse
+                    binding.validityText.setText("💪 Good Answer !");
+                }else{
+                    binding.answer2.setBackgroundColor(Color.RED);                             //rouge de réponse
+                    binding.validityText.setText("Bad answer 😢");
+                }
+                binding.validityText.setVisibility(View.VISIBLE);
+                binding.next.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewModel.isAnswerValid(2)){
+                    binding.answer3.setBackgroundColor(Color.parseColor("#388e3c"));  //vert de réponse
+                    binding.validityText.setText("💪 Good Answer !");
+                }else{
+                    binding.answer3.setBackgroundColor(Color.RED);                             //rouge de réponse
+                    binding.validityText.setText("Bad answer 😢");
+                }
+                binding.validityText.setVisibility(View.VISIBLE);
+                binding.next.setVisibility(View.VISIBLE);
+            }
+        });
+        binding.answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewModel.isAnswerValid(3)){
+                    binding.answer4.setBackgroundColor(Color.parseColor("#388e3c"));  //vert de réponse
+                    binding.validityText.setText("💪 Good Answer !");
+                }else{
+                    binding.answer4.setBackgroundColor(Color.RED);                             //rouge de réponse
+                    binding.validityText.setText("Bad answer 😢");
+                }
+                binding.validityText.setVisibility(View.VISIBLE);
+                binding.next.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void updateQuestion(Question question){
+
+        binding.question.setText(question.getQuestion());
+        binding.answer1.setText(question.getChoiceList().get(0));
+        binding.answer2.setText(question.getChoiceList().get(1));
+        binding.answer3.setText(question.getChoiceList().get(2));
+        binding.answer4.setText(question.getChoiceList().get(3));
     }
 }
